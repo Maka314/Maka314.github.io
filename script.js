42d827db5bf6388a31a07eaed28258f42f1965f3
mingchen <traveller31415@gmail.com>
2023-11-29 13:50:04 +1100

    Update placeholder values in index.html and add functionality to check if a set is a super key in script.js

--- script.js
+++ script.js
@@ -3,11 +3,65 @@ const requirementTemplet = document.querySelector('.fd_input')
 const calculateButton = document.querySelector('.calculateButton')
 let FDlist = document.querySelectorAll('.fd_input')
 
-addFD.addEventListener('click', ()=>{
+class batterSet extends Set {
+    isSubSet(setB) {
+        return [...this].every(value => setB.has(value));
+    }
+
+    union(setB) {
+        return new Set([...this, ...setB]);
+    }
+
+    isEqual(setB) {
+        return [...this].every(value => setB.has(value)) && [...setB].every(value => this.has(value));
+    }
+}
+
+addFD.addEventListener('click', () => {
     // requirementTemplet.parentNode.appendChild(requirementTemplet.cloneNode(true))
-    addFD.parentNode.insertBefore(requirementTemplet.cloneNode(true), addFD)
+    addFD.parentNode.parentNode.insertBefore(requirementTemplet.cloneNode(true), addFD.parentNode)
 })
 
-calculateButton.addEventListener('click', ()=>{
-    FDlist = document.querySelectorAll('.fd_input')
+const getFDelement = function (fdNode) {
+    const leftHand = new batterSet(fdNode.childNodes[1].value);
+    const righrHand = new batterSet(fdNode.childNodes[3].value);
+    const res = [leftHand, righrHand];
+    return res
+}
+
+const setSuperKeyOrNot = function (targetset, fds, R) {
+    let lastRound = targetset;
+    let thisRound = targetset;
+    fds.forEach((fd) => {
+        if (fd[0].isSubSet(thisRound)) {
+            thisRound = thisRound.union(fd[1]);
+        }
+    })
+
+    while (!lastRound.isEqual(thisRound)) {
+        lastRound = thisRound;
+        fds.forEach((fd) => {
+            if (fd[0].isSubSet(thisRound)) {
+                thisRound = thisRound.union(fd[1]);
+            }
+        });
+    }
+
+    return thisRound.isEqual(R);
+}
+
+// const findCandidate = function (targetset, fds, R) {
+//     const isSuperKey = 
+// }
+
+calculateButton.addEventListener('click', () => {
+    FDlist = document.querySelectorAll('.fd_input');
+    const allFDinSet = [];
+    FDlist.forEach((FDNode) => allFDinSet.push(getFDelement(FDNode)));
+    let superKey = new batterSet(allFDinSet.map((item) => [...item[0]]).flat());
+    let R = new batterSet(allFDinSet.map((item) => [...item[1]]).flat()).union(superKey);
+
+    console.log(`${Array.from(superKey)} is super key and ${Array.from(R)} is R`);
+
+    console.log(setSuperKeyOrNot(superKey, allFDinSet, R));
 })
